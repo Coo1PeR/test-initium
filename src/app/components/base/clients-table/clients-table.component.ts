43ad5984a7c64096084dbb05389d6ce522826659
Coo1PeR <coo2per@gmail.com>
2024-08-27 16:48:15 +0300

    feat: fetch data from api

--- src/app/components/base/clients-table/clients-table.component.ts
+++ src/app/components/base/clients-table/clients-table.component.ts
@@ -1,97 +1,65 @@
-import {AfterViewInit, Component, ViewChild} from '@angular/core';
-import {Users} from "../../../core/interfaces/users";
-import {MatTableDataSource} from "@angular/material/table";
-import {SelectionModel} from "@angular/cdk/collections";
-import {LiveAnnouncer} from "@angular/cdk/a11y";
-import {MatSort, Sort} from "@angular/material/sort";
+  import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
+  import {Users} from "../../../core/interfaces/users";
+  import {MatTableDataSource} from "@angular/material/table";
+  import {SelectionModel} from "@angular/cdk/collections";
+  import {MatSort} from "@angular/material/sort";
+  import {GetUsersService} from "../../../core/services/get-users.service";
+  import {Observable} from "rxjs";
 
-const USERS: Users[] = [
-  {
-    "name": "Александр",
-    "surname": "Петров",
-    "email": "petrov@mail.ru",
-    "phone": "+79061856195"
-  },
-  {
-    "name": "Павел",
-    "surname": "Прилучный",
-    "email": "ppl98@mail.ru",
-    "phone": "+79891456090"
-  },
-  {
-    "name": "Иван",
-    "surname": "Охлобыстин",
-    "email": "iohl_990@mail.ru",
-    "phone": "+79053856195"
-  },
-  {
-    "name": "Марина",
-    "surname": "Александрова",
-    "email": "malexan21@mail.ru",
-    "phone": "+79052206950"
-  },
-  {
-    "name": "Юрий",
-    "surname": "Борисов",
-    "email": "borisov@gmail.com",
-    "phone": ""
-  }
-]
+  @Component({
+    selector: 'app-clients-table',
+    templateUrl: './clients-table.component.html',
+    styleUrls: ['./clients-table.component.scss']
+  })
 
-@Component({
-  selector: 'app-clients-table',
-  templateUrl: './clients-table.component.html',
-  styleUrls: ['./clients-table.component.scss']
-})
+  export class ClientsTableComponent implements OnInit, AfterViewInit {
+    dataSource: MatTableDataSource<Users> = new MatTableDataSource<Users>([]);
+    users$: Observable<Users[]> | undefined;
+    displayedColumns: string[] = ['select', 'name', 'surname', 'email', 'phone'];
+    selection = new SelectionModel<Users>(true, []);
 
+    constructor(
+      private getUsersService: GetUsersService
+    ) {}
 
-export class ClientsTableComponent implements AfterViewInit {
-  displayedColumns: string[] = ['select', 'name', 'surname', 'email', 'phone'];
-  dataSource = new MatTableDataSource<Users>(USERS);
-  selection = new SelectionModel<Users>(true, []);
-
-  constructor(private _liveAnnouncer: LiveAnnouncer) {}
-
-  @ViewChild(MatSort) sort!: MatSort;
-
-  ngAfterViewInit() {
-    this.dataSource.sort = this.sort;
-  }
-
-  // /** Announce the change in sort state for assistive technology. */
-  // announceSortChange(sortState: Sort) {
-  //   // This example uses English messages. If your application supports
-  //   // multiple language, you would internationalize these strings.
-  //   // Furthermore, you can customize the message to add additional
-  //   // details about the values being sorted.
-  //   if (sortState.direction) {
-  //     this._liveAnnouncer.announce(`Sorted ${sortState.direction}ending`);
-  //   } else {
-  //     this._liveAnnouncer.announce('Sorting cleared');
-  //   }
-  // }
-
-  isAllSelected() {
-    const numSelected = this.selection.selected.length;
-    const numRows = this.dataSource.data.length;
-    return numSelected === numRows;
-  }
-
-  /** Selects all rows if they are not all selected; otherwise clear selection. */
-  toggleAllRows() {
-    if (this.isAllSelected()) {
-      this.selection.clear();
-      return;
+    // Get users
+    ngOnInit(): void {
+      this.users$ = this.getUsersService.getAllUsersObservable();
+      this.users$.subscribe((data) => {
+        this.dataSource.data = data ?? [];
+        if (this.sort) {
+          this.dataSource.sort = this.sort;
+        }
+      });
     }
 
-    this.selection.select(...this.dataSource.data);
-  }
+    // Sorting
+    @ViewChild(MatSort) sort!: MatSort;
 
-  /** The label for the checkbox on the passed row */
-  checkboxLabel(row?: Users): string {
-    if (!row) {
-      return `${this.isAllSelected() ? 'deselect' : 'select'} all`;
+    ngAfterViewInit() {
+      this.dataSource.sort = this.sort;
     }
-    return `${this.selection.isSelected(row) ? 'deselect' : 'select'}`; /*row ${row.position + 1}*/
+
+    // CheckBoxes
+    isAllSelected() {
+      const numSelected = this.selection.selected.length;
+      const numRows = this.dataSource.data.length;
+      return numSelected === numRows;
+    }
+
+    toggleAllRows() {
+      if (this.isAllSelected()) {
+        this.selection.clear();
+        return;
+      }
+
+      this.selection.select(...this.dataSource.data);
+    }
+
+    checkboxLabel(row?: Users): string {
+      if (!row) {
+        return `${this.isAllSelected() ? 'deselect' : 'select'} all`;
+      }
+      return `${this.selection.isSelected(row) ? 'deselect' : 'select'}`;
+    }
   }
-}
--- src/app/core/services/get-users.service.ts
+++ src/app/core/services/get-users.service.ts
@@ -1,17 +1,69 @@
-import {Injectable} from '@angular/core';
-import {HttpClient} from "@angular/common/http";
-import {Observable} from "rxjs";
-import {Users} from "../interfaces/users";
+import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { from, map, Observable } from "rxjs";
+import { Users } from "../interfaces/users";
+import { openDB, IDBPDatabase } from 'idb';
+
+const DB_NAME = 'UsersDB';
+const STORE_NAME = 'users';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GetUsersService {
-  private URL: string =  'https://test-data.directorix.cloud/task1';
+  private URL: string = 'https://test-data.directorix.cloud/task1';
+  private dbPromise: Promise<IDBPDatabase>;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {
+    this.dbPromise = this.initDB();
+  }
 
-  getUsers(): Observable<Users[]> {
-    return this.http.get<Users[]>(`${this.URL}`);
+  private async initDB() {
+    return openDB(DB_NAME, 1, {
+      upgrade(db) {
+        if (!db.objectStoreNames.contains(STORE_NAME)) {
+          db.createObjectStore(STORE_NAME, {keyPath: 'email'});
+        }
+      },
+    });
+  }
+
+  fetchUsersFromAPI(): Observable<Users[]> {
+    return this.http.get<{ users: Users[] }>(this.URL)
+      .pipe(
+        // Extract users array from the response
+        map(response => response.users)
+      );
+  }
+
+  async getAllUsers(): Promise<Users[]> {
+    const db = await this.dbPromise;
+    const users = await db.getAll(STORE_NAME);
+
+    // if DB is empty, fetch users
+    if (users.length === 0) {
+      const fetchedUsers = await this.fetchUsersFromAPI().toPromise();
+
+      // if users is fetched, save them in indexedDB and return
+      if (fetchedUsers && fetchedUsers.length > 0) {
+        for (const user of fetchedUsers) {
+          await this.addUser(user);
+        }
+        return fetchedUsers;
+      } else {
+        // Return empty array if there is no users
+        return [];
+      }
+    }
+    return users;
+  }
+
+  getAllUsersObservable(): Observable<Users[]> {
+    return from(this.getAllUsers());
+  }
+
+  async addUser(user: Users): Promise<void> {
+    const db = await this.dbPromise;
+    await db.put(STORE_NAME, user);
   }
 }
