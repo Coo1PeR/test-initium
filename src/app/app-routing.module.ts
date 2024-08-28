fff1a4aebf0c13f5b12b81b805be1186c3712d14
Coo1PeR <coo2per@gmail.com>
2024-08-28 21:58:50 +0300

    feat: add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BaseComponent } from "./components/base/base.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/base',
+    pathMatch: 'full'
+  },
+  {
+    path: 'base',
+    component: BaseComponent,
+    title: 'Клиенты',
+  },
+  {
+    path: '**',
+    redirectTo: '/base',
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
